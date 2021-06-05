991a7a7941fa6e0cae3f407ce0433db24e822aa1
Dimitar.Petroww <Mitko66692@abv.bg>
2021-06-05 15:49:52 +0300

    update

--- controllers/create.js
+++ controllers/create.js
@@ -2,7 +2,19 @@ module.exports = {
     GET: (req, res) => {
         res.render("create")
     },
-    POST: (req, res) => {
-        console.log(req.body);
+    POST: async (req, res) => {
+        const cube = {
+            name: req.body.name,
+            description: req.body.description,
+            imageURL: req.body.imageURL,
+            difficulty: Number(req.body.difficulty)
+        }
+        try {
+            await req.cubeStorage.create(cube)
+
+            res.redirect("/")
+        }catch(e) {
+            res.render("create", {error: e.message})
+        }
     }
 }
--- models/Cube.js
+++ models/Cube.js
@@ -1,10 +1,10 @@
 const { Schema, model} = require("mongoose")
 
 const schema = new Schema({
-    name: { type: String, required: true},
-    description: { type: String, required: true, minLength: 10},
-    imageURL: { type: String, required: true, match: /^https?/},
-    difficulty: { type: Number, required: true, min: 1, max: 6}
+    name: { type: String, required: [true, "Name is required"]},
+    description: { type: String, required: [true, "Description is required"], minLength: 10},
+    imageURL: { type: String, required: [true, "ImageURL is required"], match: /^https?/},
+    difficulty: { type: Number, required: [true, "Difficulty level is required"], min: 1, max: 6}
 })
 
 module.exports = model("Cube", schema)
